23c4d4a4846a12b1f9b0173eb3caa2db19a4489f
João Pedro <jaumassis@gmail.com>
2025-01-03 12:40:39 -0300

    first commit

---
+++ Operador LIMIT.sql
@@ -0,0 +1,10 @@
+-- LIMIT limita a quantidade de linhas que o resultado irá trazer, ou quais registros eu quero que apareça no resultado.
+
+SELECT *
+FROM actor
+LIMIT 10
+
+--Exemplo 2--------------------------------------------------------------------------------------------------------------------------
+SELECT *
+FROM actor
+LIMIT 5, 10 -- me mostra os 10 resultados A PARTIR do 6º.
